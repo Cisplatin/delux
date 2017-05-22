0cd6a98a8d7d4ac5e8861319e4b722aad85a5497
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-05-22 19:38:43 -0400

    first draft finished

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ git clone https://github.com/Cisplatin/delux
 cd ./delux
 pip install -r requirements.txt
 ```
+
+To add personal settings to Delux, create a `settings.yaml` file with the following format:
+
+```
+admin_number: 16471234567
+building_number: 16471234569
+whitelist:
+  16471234567: thom yorke
+  16471234568: jonny greenwood
+```
--- run.py
+++ run.py
@@ -1,24 +1,51 @@
+from datetime import datetime
 from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse
+from yaml import load, YAMLError
 
 app = Flask(__name__)
+settings = None
+unlocked_until = datetime.now()
 
-ADMIN = '+14168587766'
-WHITELIST = {
-    "+14168587766" : "Simon Hajjar",
-}
+# @return [Boolean] True if the door is set to be unlocked, false otherwise.
+def unlocked():
+    return datetime.now() < unlocked_until:
 
-@app.route("/", methods=['GET', 'POST'])
+# @param number [String] The number to check for whitelist.
+# @return [Boolean] True if the given number is whitelisted.
+def whitelisted(number):
+    return number in settings['whitelist']
+
+# @param number [String] The number to check against the building.
+# @return [Boolean] True if the given number belongs to the building.
+def building_number(number):
+    return number == settings['building_number']
+
+@app.route('/', methods=['GET', 'POST'])
 def incoming_call():
     from_number = request.values.get('From', None)
     resp = VoiceResponse()
 
-    if from_number in callers:
-        resp.say("Opening door.")
+    # If the number is the building's and the door is unlocked, allow entry.
+    if unlocked() and building_number(from_number):
+        play = ('w' * settings['pause_time']) + settings['digit']
+        resp.play(digits=play)
+
+    # If the number is whitelisted, allow for unlocking.
+    elif whitelisted(number):
+        time_delta = datetime.timedelta(minutes = settings['door_delay'])
+        unlocked = datetime.now() + time_delta
+
+    # If the number is not white-listed, redirect to the admin.
     else:
-        resp.say("Not opening door.")
+        resp.dial(settings['admin_number'])
 
     return str(resp)
 
 if __name__ == "__main__":
+    with open('settings.yaml', 'r') as stream:
+        try:
+            settings = load(stream)
+        except YAMLError as error:
+            raise EnvironmentError('Malformed settings.yaml file.')
     app.run(debug=True)
